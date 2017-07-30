7d1effa648b2549f131712365ebf60dd83b5f601
Wolfgang Lutz <wlut@num42.de>
2017-07-30 12:57:20 +0200

    vastly reduce FolderManager Code

--- wallpaperTimer/Manager/FolderManager.swift
+++ wallpaperTimer/Manager/FolderManager.swift
@@ -6,29 +6,18 @@
 // Copyright © 2015 wlutz. All rights reserved.
 //
 
-import Cocoa
+import Foundation
 
 struct FolderManager {
   let baseFolder: URL
 
-  func folderUrlUsingDaytime(_ useDaytime: Bool) -> URL {
-
-    let folderName: String
-
-    if useDaytime {
-      folderName = DayTime.currentDayTimeName
-    } else {
-      folderName = "all"
-    }
-
-    return baseFolder.appendingPathComponent(folderName)
-  }
-
   func groupsUsingDaytime(_ useDaytime: Bool) -> [WallpaperGroup] {
 
-    let daytimeFolders = FileManager.default.visibleFolderURLsAtURL(folderUrlUsingDaytime(useDaytime))
-    let allFolders = FileManager.default.visibleFolderURLsAtURL(folderUrlUsingDaytime(false))
-
-    return (daytimeFolders + allFolders).flatMap { WallpaperGroup(groupFolderURL: $0) }
+    return [
+      baseFolder.appendingPathComponent(DayTime.currentDayTimeName),
+      baseFolder.appendingPathComponent("all")
+    ]
+    .flatMap { FileManager.default.visibleFolderURLsAtURL($0) }
+    .flatMap { WallpaperGroup(groupFolderURL: $0) }
   }
 }
